f60476150691d3106a318bfd87e59c01ab0c184b
Cici <cici.x.wang@gmail.com>
2020-03-24 21:34:54 -0400

    cici_ai

--- cici_strategy.py
+++ cici_strategy.py
@@ -10,4 +10,13 @@ def cici_strategy(your_health, your_potion_ct, oppt_health, turn_number):
     AI.valid_actions["PASS"],
   ]
 
-  return random.choice(actions)
+  if your_health <= 10 and your_potion_ct > 0:
+  	return "POTION"
+
+  if your_health >= oppt_health + 3:
+  	return "ATTACK"
+
+  if turn_number%4 == 0:
+  	return "ATTACK"
+
+  return "BLOCK"
